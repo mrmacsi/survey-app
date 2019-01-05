fb82640978b8f99fa1427f6a755b4adb9ae5519d
Macit Simsek <macitsimsek12@hotmail.com>
2019-01-05 20:43:14 +0000

    get the port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({hello:'buddy'});
+    res.send({hello:process.env.PORT});
 });
 
 const PORT = process.env.PORT || 5000;
