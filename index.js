5e176d84eba30375c2aa8216ba3ef526d2a8fc6e
Macit Simsek <macitsimsek12@hotmail.com>
2019-01-06 13:36:46 +0000

    add cookie and session to store the user nfo

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
-require("./services/passport");
 require("./models/User");
+require("./services/passport");
 
 mongoose.connect(
   keys.mongoURI,
@@ -10,6 +12,16 @@ mongoose.connect(
 );
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //1 month
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile :", profile);
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          console.log("already exists");
+          done(null, existingUser);
+        } else {
+          new User({ googleID: profile.id, name: profile.displayName })
+            .save()
+            .then(user => done(null, user));
+          console.log("new user created");
+        }
+      });
     }
   )
 );
