7c431d820291c71a01d44d6219903602b4519e8d
Macit Simsek <macitsimsek12@hotmail.com>
2019-01-06 03:01:06 +0000

    add google auth via passportjs and add mongoosejs for mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./services/passport");
+
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({hello:process.env.PORT});
-});
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
